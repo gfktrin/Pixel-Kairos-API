8934da074e62306ba88a668b27226083ca92df04
Gabriel Trindade <gfktrin@yahoo.com.br>
2019-04-17 14:35:37 -0300

    CourseSeeder update

--- database/seeds/CourseSeeder.php
+++ database/seeds/CourseSeeder.php
@@ -13,6 +13,8 @@ class CourseSeeder extends Seeder
     {
         DB::table('courses')->insert([
             'name' => 'Desenvolvimento de games',
+            'slug' => 'desenvolvimento-de-games',
+            'description' => 'Já sonhou em criar seu proprio jogo? Com esse curso, você vai aprender a criar jogos de diferentes tipos com várias ferramentas, de forma que qualquer um possa tirar seu projeto do papel.',
         ]);
     }
 }
